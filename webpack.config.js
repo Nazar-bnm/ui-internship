28ef31251b64d42cf621cce7c3166175a42ee2cc
ovasyliev <Oleksandr_Vasyliev@jabil.com>
2019-09-22 18:31:04 +0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+
+const path = require('path');
+
 module.exports = {
   entry: ['./src/app/index.js'],
   output: {
-    path: __dirname,
-    publicPath: '/',
+    path: path.join(__dirname, './dist'),
     filename: 'bundle.js'
   },
   module: {
