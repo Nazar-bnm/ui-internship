083a51c0c75161964c1f5c2e0d4c2c60d823463f
KhrystynaVaskovych <khrystynavaskovych@gmail.com>
2019-09-25 16:05:05 +0300

    Minor changes with indentation

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -2,25 +2,24 @@ import { combineReducers } from 'redux';
 import { INCREMENT, DECREMENT } from '../actions/actionTypes';
 
 const initialState = {
-    count: 0,
+  count: 0,
 };
 
 function reducer(state = initialState, action) {
-    switch(action.type) {
-      case INCREMENT:
-        return {
-            count: state.count + 1
-        };
-      case DECREMENT:
-        return {
-            count: state.count - 1
-        };
-      default:
-        return state;
-    }
+  switch(action.type) {
+    case INCREMENT:
+      return {
+        count: state.count + 1
+      };
+    case DECREMENT:
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
 }
 
 export default combineReducers({
-    reducer
+  reducer
 });
-
