afbd139a3b1bd2070bbab8886b1d88c82854e797
Andrii Veremchuk <andveremchuk@gmail.com>
2019-09-26 14:47:28 +0300

    added users component

--- src/app/service/httpService.js
+++ src/app/service/httpService.js
@@ -19,40 +19,40 @@ export default class httpService {
   async get(url){
       try {
       const response = await axios.get(url, this.config);
-      console.log(response);
+      return response.data;
     }
     catch (error) {
-      console.log(error.response.data);
+      return error.response.data;
     }
   }
 
   async post(url, data){
     try {
       const response = await axios.post(url, data, this.config);
-      console.log(response);
+      return response.data;
     }
     catch (error) {
-      console.log(error.response.data);
+      return error.response.data;
     }
   }
 
   async put(url, data){
     try {
       const response = await axios.put(url, data, this.config);
-      console.log(response);
+      return response.data;
     }
     catch (error) {
-      console.log(error.response.data);
+      return error.response.data;
     }
   }
 
   async delete(url){
     try {
       const response = await axios.delete(url, this.config);
-      console.log(response);
+      return response.data;
     }
     catch (error) {
-      console.log(error.response.data);
+      return error.response.data;
     }
   }
 };
