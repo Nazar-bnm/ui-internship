f1fb6306909b1aaeea91f291422c1667099a79ad
vantonov <vantonov@griddynamics.com>
2019-09-26 19:32:07 +0300

    Packages added

--- src/app/service/httpService.spec.js
+++ src/app/service/httpService.spec.js
@@ -1,15 +1,15 @@
-import httpService from './httpService'
+import httpService from './httpService';
 
 describe('httpService', () => {
   let storeAPI;
   beforeEach(() => {
     storeAPI = new httpService();
-  })
+  });
 
   test('storeAPI should be an instance of httpService', () =>
     expect(storeAPI instanceof httpService).toBe(true));
 
   test('storeAPI should have method post', () => {
     expect(storeAPI.post !== 'undefined').toBe(true);
-  })
-})
+  });
+});
