201036c0fe6170b2c6827b68070419d80f22fcae
Vasyl <vandrela@griddynamics.com>
2019-10-02 10:51:09 +0300

    join-us

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React from 'react';
+import JoinUs from '../JoinUs/JoinUs';
 
 const Footer = () => (
   <div>
     <h2>Footer</h2>
+    <JoinUs />
   </div>
 );
 
