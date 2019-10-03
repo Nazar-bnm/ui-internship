c0c6534d5d3fd0558e73cb083cde8c975d746864
mvasylykiv <mvasylykiv@griddynamics.com>
2019-10-03 11:42:40 +0300

    burger menu 50% done

--- src/components/Header/BurgerMenu/BurgerMenu.js
+++ src/components/Header/BurgerMenu/BurgerMenu.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import Button from '../../Button';
+import './BurgerMenu.scss';
 
 const BurgerMenu = () => {
   return (
     <div className="burger-menu-container">
-      <Button icon="bars" customClass="burger-menu-btn"></Button>
+      <nav>
+        <input type="checkbox" id="burgerbutton" />
+        <label htmlFor="burgerbutton" className="burgerimage" title="Navigation">&#x2630;</label>
+        <ul className="burger-list">
+          <li>
+            <a className="burger-menu-link" href="#" title="Link">Link</a>
+          </li>
+          <li>
+            <a className="burger-menu-link" href="#" title="Link">Link</a>
+          </li>
+          <li>
+            <a className="burger-menu-link" href="#" title="Link">Link</a>
+          </li>
+          <li>
+            <a className="burger-menu-link" href="#" title="Link">Link</a>
+          </li>
+          <li>
+            <a className="burger-menu-link" href="#" title="Link">Link</a>
+          </li>     
+        </ul>
+      </nav>
     </div>
   );
 };
