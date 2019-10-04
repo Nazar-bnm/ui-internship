7a45b351e3eee187927b6cee77a41fbfe83657a6
mvasylykiv <mvasylykiv@griddynamics.com>
2019-10-04 14:17:11 +0300

    Changed name of component

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import PageSettings from './PageSettings';
+import Button from '../Button';
+import PageNavigation from './PageNavigation';
+import BurgerMenu from '../BurgerMenu';
+import Input from '../Input';
+import { burgerMenuItemsList, navConfig } from '../../config/headerMockups';
 import { Link } from 'react-router-dom';
-
-const Header = () => {
+import './Header.scss';
+const Header = (props) => {
   return (
-    <div>
-      <h1>Header</h1>
-      <ul>
-        <li>
-          <Link to="/home">Home Page</Link>
-        </li>
-        <li>
-          <Link to="/products">Product List Page</Link>
-        </li>
-        <li>
-          <Link to="/counter">Counter</Link>
-        </li>
-      </ul>
-    </div>
+    <header className="header">
+      <div className="wrapper color-secondary">
+        <PageSettings/>
+      </div>
+      <div className="heading-search-cart-container content">
+        <Link to={'/home'}>
+          <h2 className="header__title">The shop</h2>
+        </Link>
+        <div className="buttons-container">
+          <BurgerMenu menuItemsList={burgerMenuItemsList}/>
+          <Input icon="search" customClass="search-input">Search</Input>
+          <Link to={'/404'}>
+            <Button icon="cart" customClass="cart-btn">Cart (0)</Button>
+          </Link>
+        </div>
+      </div>
+      <PageNavigation options={navConfig} />
+      <div className="wrapper _slider">Here will be slider</div>
+    </header>
   );
 };
-
 export default Header;
