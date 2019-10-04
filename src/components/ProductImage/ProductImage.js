6d674d4cd7922e8858bd5ee5f6832cb3d87e9895
myrosvas <myrosvas@gmail.com>
2019-10-04 22:10:44 +0300

    images layout added

--- src/components/ProductImage/ProductImage.js
+++ src/components/ProductImage/ProductImage.js
@@ -6,24 +6,24 @@ const ProductImage = (imagesArr) => {
     <div className="product-image-container">
       <figure className="big-image-container">
         <img src={imagesArr[0].src} className="product-image" />
-        <div class="image-on-hover"></div>
+        <div className="image-on-hover"></div>
       </figure>
       <div className="small-images-container">
         <figure className="small-image">
           <img src={imagesArr[1].src} className="product-image" />
-          <div class="image-on-hover"></div>
+          <div className="image-on-hover"></div>
         </figure>
         <figure className="small-image">
           <img src={imagesArr[2].src} className="product-image" />
-          <div class="image-on-hover"></div>
+          <div className="image-on-hover"></div>
         </figure>
         <figure className="small-image">
           <img src={imagesArr[3].src} className="product-image" />
-          <div class="image-on-hover"></div>
+          <div className="image-on-hover"></div>
         </figure>
         <figure className="small-image">
           <img src={imagesArr[3].src} className="product-image" />
-          <div class="image-on-hover"></div>
+          <div className="image-on-hover"></div>
         </figure>
       </div>
     </div>
