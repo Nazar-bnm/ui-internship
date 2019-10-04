262f8d4143e62d627418c4995f82a4df39441f55
myrosvas <myrosvas@gmail.com>
2019-10-04 22:07:59 +0300

    images layout added

--- src/components/ProductImage/ProductImage.js
+++ src/components/ProductImage/ProductImage.js
@@ -4,22 +4,27 @@ import './ProductImage.scss';
 const ProductImage = (imagesArr) => {
   return (
     <div className="product-image-container">
-      <div className="big-image-container">
+      <figure className="big-image-container">
         <img src={imagesArr[0].src} className="product-image" />
-      </div>
+        <div class="image-on-hover"></div>
+      </figure>
       <div className="small-images-container">
-        <div className="small-image">
+        <figure className="small-image">
           <img src={imagesArr[1].src} className="product-image" />
-        </div>
-        <div className="small-image">
+          <div class="image-on-hover"></div>
+        </figure>
+        <figure className="small-image">
           <img src={imagesArr[2].src} className="product-image" />
-        </div>
-        <div className="small-image">
+          <div class="image-on-hover"></div>
+        </figure>
+        <figure className="small-image">
           <img src={imagesArr[3].src} className="product-image" />
-        </div>
-        <div className="small-image">
+          <div class="image-on-hover"></div>
+        </figure>
+        <figure className="small-image">
           <img src={imagesArr[3].src} className="product-image" />
-        </div>
+          <div class="image-on-hover"></div>
+        </figure>
       </div>
     </div>
   );
