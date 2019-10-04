4eeb2844c33c7328145e851353924393786a7fff
mvasylykiv <mvasylykiv@griddynamics.com>
2019-10-04 14:06:39 +0300

    Fixes of header and test

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,35 +1,22 @@
 import React from 'react';
-import PageSettings from './PageSettings';
-import Button from '../Button';
-import PageNavigation from './PageNavigation';
-import BurgerMenu from '../BurgerMenu';
-import navConfig from '../../config/headerMockups';
-import Input from '../Input';
-import { burgerMenuItemsList } from '../../config/headerMockups';
 import { Link } from 'react-router-dom';
-import './_header.scss';
 
-const Header = (props) => {
+const Header = () => {
   return (
-    <header className="header">
-      <div className="wrapper color-secondary">
-        <PageSettings/>
-      </div>
-      <div className="heading-search-cart-container content">
-        <Link to={'/home'}>
-          <h2 className="header__title">The shop</h2>
-        </Link>
-        <div className="buttons-container">
-          <BurgerMenu menuItemsList={burgerMenuItemsList}/>
-          <Input icon="search" customClass="search-input">Search</Input>
-          <Link to={'/404'}>
-            <Button icon="cart" customClass="cart-btn">Cart (0)</Button>
-          </Link>
-        </div>
-      </div>
-      <PageNavigation options={navConfig} />
-      <div className="wrapper _slider">Here will be slider</div>
-    </header>
+    <div>
+      <h1>Header</h1>
+      <ul>
+        <li>
+          <Link to="/home">Home Page</Link>
+        </li>
+        <li>
+          <Link to="/products">Product List Page</Link>
+        </li>
+        <li>
+          <Link to="/counter">Counter</Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
