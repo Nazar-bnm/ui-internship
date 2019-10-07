89903a6bf059df5aff6d10087a2a192f883db14e
mvasylykiv <mvasylykiv@griddynamics.com>
2019-10-07 18:08:27 +0300

    layout added

--- src/components/BlogArticlePreview/BlogArticlePreview.js
+++ src/components/BlogArticlePreview/BlogArticlePreview.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './BlogArticlePreview.scss';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+export const CN = 'article-preview';
 
 const BlogArticlePreview = () => {
   return (
-    <div className="blog-article-preview-container">
-      <div className="title-and-text-container">
-        <h2>best summer trends</h2>
-        <p>we know what you need this summer. most fashion looks</p>
+    <div className={cx(CN)}>
+      <div className={`${CN}__content`}>
+        <h2 className={`${CN}__heading`}>best summer trends</h2>
+        <p className={`${CN}__text`}>we know what you need this summer. most fashion looks</p>
       </div>
     </div>
   );
+};
+
+BlogArticlePreview.propTypes = {
+  className: PropTypes.string,
 };
 
 export default BlogArticlePreview;
