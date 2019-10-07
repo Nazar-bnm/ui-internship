59de6ed08d7536da5d47e4496146bc96efdea209
Vasyl <vandrela@griddynamics.com>
2019-10-07 17:28:06 +0300

    added component most popular with styles

--- src/components/MostPopular/ProductItem/ProductItem.js
+++ src/components/MostPopular/ProductItem/ProductItem.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './ProductItem.scss';
+import cx from 'classnames';
+
 
 const ProductItem = ({ product }) => {
-  const addLabel = ({ label }) => {
-    // if (label === 'new') {
-    //   return (
-    //     <div className="product__img-wrapper__flag">{label}</div>
-    //   );
-    // } else if (label === 'sale') {
-    //   return (
-    //     <div className="product__img-wrapper__flag red">{label}</div>
-    //   );
-    // }
-    label === 'new' ? <div className="product__img-wrapper__flag">{label}</div> :
-      label === 'sale' ? <div className="product__img-wrapper__flag red">{label}</div> :
-        <div className="product__img-wrapper__flag transparent">{label}</div>;
-  };
-
   const { image, label, title, price } = product;
+
   return (
     <div className="product col-4">
       <div className="product__img-wrapper">
-        <div className="product__img-wrapper__flag">{label}</div>
+        <div className={cx('product__img-wrapper__flag', { red: (label === 'sale') },
+          { black: (label === 'new' ) })}></div>
+        <div className='product__img-wrapper__labels'>{label}</div>
         <img className="product__img-wrapper__img" src={image}/>
       </div>
       <div className="product__title-wrapper">
-        <h1 className="product__title-wrapper__title">{title}</h1>
-        <h2 className="product__title-wrapper__price">{price}</h2>
+        <h2 className="product__title-wrapper__title">{title}</h2>
+        <h3 className="product__title-wrapper__price">{price}</h3>
       </div>
     </div>
   );
