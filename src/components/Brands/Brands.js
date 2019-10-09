8ff6625960d3e5c62ebf3295d381a866079004e8
Olena Kachmar <okachmar@griddynamics.com>
2019-10-09 17:35:52 +0300

    brands section done

--- src/components/Brands/Brands.js
+++ src/components/Brands/Brands.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import HttpService from '../../service/HttpService/httpService';
+import cx from 'classnames';
+
 import './Brands.scss';
+
+export const CN = 'shippingInfo';
 
 class BrandsAndShippingInfo extends Component {
   state = {
-    items: [],
+    brandsList: [],
   };
 
   async downloadProducts() {
     const userAPI = new HttpService();
     const response = await userAPI.get('http://localhost:4000/brands');
-    this.setState({ items: response.data });
+    this.setState({ brandsList: response.data });
   }
 
   componentDidMount() {
@@ -18,43 +22,38 @@ class BrandsAndShippingInfo extends Component {
   }
 
   render() {
-    const { items } = this.state;
-    const block = items.map((item) => (
-      <li key={item.id} className="brandItem">
-        <img src={item.photo} className="brandItemPhoto"></img>
+    const shippingData = [
+      { number: 1, title: 'free shipping', description: 'on all orders over 50$' },
+      { number: 2, title: 'money back guarantee', description: 'on all orders' },
+      { number: 3, title: 'worldwide delivery', description: 'to over 80 countries' },
+
+    ];
+    const { brandsList } = this.state;
+    const brands = brandsList.map((brand) => (
+      <li key={brand.id} className="col-2 brand__list-item">
+        <img src={brand.photo} className="brand__list-photo"></img>
+      </li>
+    ));
+    const shippingInfo = shippingData.map((elem) => (
+      <li key={elem.number} className={`${CN}__list-item`}>
+        <span className={`${CN}__item-number`}>0{elem.number}.</span>
+        <div className={`${CN}__item-text__wrapper`}>
+          <h5 className={`${CN}__item-text__title`}>{elem.title}</h5>
+          <h6 className={`${CN}__item-text__description`}>{elem.description}</h6>
+        </div>
       </li>
     ));
 
     return (
       <div className="content">
         <h3 className="title">Top Brands</h3>
-        <div className="container">
-          <ul className="brandsList">
-            {block}
+        <div className="container brand">
+          <ul className="brand__list">
+            {brands}
           </ul>
           <hr className="line"/>
-          <ul>
-            <li>
-              <span>01.</span>
-              <div>
-                <h5>free shipping</h5>
-                <h6>on all orders over 50$</h6>
-              </div>
-            </li>
-            <li>
-              <span>02.</span>
-              <div>
-                <h5>money back guarantee</h5>
-                <h6>on all orders</h6>
-              </div>
-            </li>
-            <li>
-              <span>03.</span>
-              <div>
-                <h5>worldwide delivery</h5>
-                <h6>to over 80 countries</h6>
-              </div>
-            </li>
+          <ul className={`${cx(CN)}__list`}>
+            {shippingInfo}
           </ul>
         </div>
       </div>
