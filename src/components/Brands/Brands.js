471322f3e16c5adf2e9154e70e626bd0ae6bce43
Olena Kachmar <olena.kachmar9@gmail.com>
2019-10-09 09:56:26 +0300

    in progres

--- src/components/Brands/Brands.js
+++ src/components/Brands/Brands.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import HttpService from '../../service/HttpService/httpService';
+import './Brands.scss';
 
 class BrandsAndShippingInfo extends Component {
   state = {
     items: [],
-  }
+  };
 
   async downloadProducts() {
     const userAPI = new HttpService();
@@ -19,16 +20,41 @@ class BrandsAndShippingInfo extends Component {
   render() {
     const { items } = this.state;
     const block = items.map((item) => (
-      <li key={item.id}>
-        <img src={item.photo} className="brandItem"></img>
+      <li key={item.id} className="brandItem">
+        <img src={item.photo} className="brandItemPhoto"></img>
       </li>
     ));
+
     return (
       <div className="content">
         <h3 className="title">Top Brands</h3>
         <div className="container">
           <ul className="brandsList">
             {block}
+          </ul>
+          <hr className="line"/>
+          <ul>
+            <li>
+              <span>01.</span>
+              <div>
+                <h5>free shipping</h5>
+                <h6>on all orders over 50$</h6>
+              </div>
+            </li>
+            <li>
+              <span>02.</span>
+              <div>
+                <h5>money back guarantee</h5>
+                <h6>on all orders</h6>
+              </div>
+            </li>
+            <li>
+              <span>03.</span>
+              <div>
+                <h5>worldwide delivery</h5>
+                <h6>to over 80 countries</h6>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
