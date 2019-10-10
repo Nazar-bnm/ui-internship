4cac3ef40c4c5d38115ad568ee2ea3b6d841006a
mvasylykiv <mvasylykiv@griddynamics.com>
2019-10-10 17:41:42 +0300

    basic layout added

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ProductList from '../ProductList';
 
 const HomePage = () => (
   <div>
     <h2>HomePage</h2>
+    <ProductList />
   </div>
 );
 
---
+++ src/components/ProductList/ProductList.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import cx from 'classnames';
+import Dropdown from '../Dropdown';
+import { options } from '../../config/headerMockups';
+import './ProductList.scss';
+export const CN = 'product-list';
+
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className={CN}>
+        <div className={`${CN}__filter-wrapper-top`}>
+          <div className={`${CN}__dropdowns-labels-wrapper`}>
+            <span className={`${CN}__dropdowns-labels`}>price</span>
+            <Dropdown options={options.company} />
+          </div>
+          <div className={`${CN}__dropdowns-labels-wrapper`}>
+            <span className={`${CN}__dropdowns-labels`}>position</span>
+            <Dropdown options={options.company} />
+          </div>
+          <div className={`${CN}__dropdowns-labels-wrapper`}>
+            <span className={`${CN}__dropdowns-labels`}>name</span>
+            <Dropdown options={options.company} />
+          </div>
+          <div className={`${CN}__dropdowns-labels-wrapper`}>
+            <span className={`${CN}__dropdowns-labels`}>items on page</span>
+            <Dropdown options={options.company} />
+          </div>
+        </div>
+        <div className={`${CN}__list-items-container`}></div>
+        <div className={`${CN}__filter-wrapper-top`}>
+          <div className={`${CN}__dropdowns-labels-wrapper`}>
+            <span className={`${CN}__dropdowns-labels`}>price</span>
+            <Dropdown options={options.company} />
+          </div>
+          <div className={`${CN}__dropdowns-labels-wrapper`}>
+            <span className={`${CN}__dropdowns-labels`}>position</span>
+            <Dropdown options={options.company} />
+          </div>
+          <div className={`${CN}__dropdowns-labels-wrapper`}>
+            <span className={`${CN}__dropdowns-labels`}>name</span>
+            <Dropdown options={options.company} />
+          </div>
+          <div className={`${CN}__dropdowns-labels-wrapper`}>
+            <span className={`${CN}__dropdowns-labels`}>items on page</span>
+            <Dropdown options={options.company} />
+          </div>
+          <div className={`${CN}__page-switcher`}>1 2 3 4 5 ... 10</div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default ProductList;
