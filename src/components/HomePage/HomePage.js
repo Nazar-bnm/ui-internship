57607520e65605dc4111c864d5890a8b80d8a908
Olena Kachmar <okachmar@griddynamics.com>
2019-10-11 19:39:06 +0300

    layout sketch

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,12 +3,16 @@ import React from 'react';
 import ProductList from '../ProductList';
 import WhatIsNew from '../WhatIsNew';
 import BlogArticlePreview from '../BlogArticlePreview';
+import MostPopular from '../MostPopular';
 
 const HomePage = () => (
   <div>
     <WhatIsNew />
+    <div className="container content">
+      <MostPopular />
+      <BlogArticlePreview />
+    </div>
     <ProductList />
-    <BlogArticlePreview />
   </div>
 );
 
