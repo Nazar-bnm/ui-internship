4910c4b3fa0271b4595a8dbe0851185b6e428ba2
VasyaAndrela <34165711+VasyaAndrela@users.noreply.github.com>
2019-10-11 18:35:22 +0300

    [UIPO-30] - Most popular component (#23)
    * init commit
    
    * added component most popular with styles
    
    * new sizes for imgs
    
    * added CN
    
    * final fixes
    
    * added hover component and fixed bug
    
    * fixed comments added transitions to hover
    
    * added description to propTypes
    
    * fixed propsTypes

---
+++ src/components/MostPopular/MostPopular.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import HttpService from '../../service/HttpService/httpService';
+import ProductItem from './ProductItem/ProductItem';
+import './MostPopular.scss';
+
+const urlBase = 'http://localhost:4000/most-popular';
+
+class MostPopular extends Component {
+  state = {
+    products: [],
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  async getProducts() {
+    const userApi = new HttpService();
+    try {
+      const response = await userApi.get(urlBase);
+      if (response && response.data) {
+        this.setState({ products: response.data });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  renderProducts() {
+    const { products } = this.state;
+
+    return products.map((product) => (
+      <ProductItem key={product.id} product={product} />
+    ));
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <h1 className="wrapper__title">Most Popular</h1>
+        <div className="wrapper__container">
+          {this.renderProducts()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MostPopular;
---
+++ src/components/MostPopular/ProductItem/ProductItem.js
@@ -0,0 +1,72 @@
+import React, { Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import './ProductItem.scss';
+
+const CN = 'product';
+const ProductItem = ({ product }) => {
+  const [isHovered, setHovered] = useState(false);
+  const { image, label, title, price, sizes } = product;
+
+  const renderView = (isHovered) => (
+    <Fragment>
+      <div className={cx(`${CN}__img-wrapper`, { [`${CN}__img-wrapper--hovered`]: isHovered })}>
+        {!isHovered && (
+          <>
+            <div className={cx(`${CN}__img-wrapper__flag`, label)}/>
+            <div className={`${CN}__img-wrapper__labels`}>{label}</div>
+          </>
+        )}
+
+        <img
+          className={cx(`${CN}__img-wrapper__img`, { [`${CN}__img-wrapper__img--hovered`]: isHovered })}
+          src={image}
+        />
+      </div>
+
+      <div className={cx(`${CN}__title-wrapper`, { [`${CN}__title-wrapper__title`]: isHovered })}>
+        <h2 className={cx(`${CN}__title-wrapper__title`, { [`${CN}__title-wrapper__title--hovered`]: isHovered })}>
+          {title}
+        </h2>
+
+        {!isHovered && <h3 className={`${CN}__title-wrapper__price`}>{price}</h3>}
+        {isHovered && (
+          <>
+            <span className='sizes'>Sizes: {sizes}</span>
+            <div className={`{${CN}__title-wrapper icons`}>
+              <i className="eye icon" />
+              <i className="cart plus icon" />
+              <i className="heart outline icon" />
+            </div>
+          </>
+        )}
+      </div>
+    </Fragment>
+  );
+
+  const handleMouseEnter = () => setHovered(true);
+  const handleMouseLeave = () => setHovered(false);
+
+  return (
+    <div
+      className={`${CN} col-xs-12 col-sm-6 col-md-4`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {renderView(isHovered)}
+    </div>
+  );
+};
+
+ProductItem.propTypes = {
+  product: PropTypes.shape({
+    image: PropTypes.string,
+    label: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.string,
+    sizes: PropTypes.string,
+  }),
+};
+
+export default ProductItem;
