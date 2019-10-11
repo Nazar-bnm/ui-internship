b537ca466ee2acddc8226ef0f157023f6e4b7ba1
Dmytro Hnatkivskyy <47508843+dimas0211@users.noreply.github.com>
2019-10-11 17:11:17 +0300

    [UIPO-15] Product items carousel component (#32)
    * main functionality, layout and fixes made
    
    * Carousel component made reusable
    
    * destructuring, fixes, additional functions moved to helpers
    
    * helpers folder renamed to carouselHelpers
    
    * created export/import, carouselHelper files in helpers
    
    * fixed problem with export/imports in helpers

---
+++ src/components/Carousel/Slide/Slide.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import './Slide.scss';
+
+import cx from 'classnames';
+
+export const CN = 'slide';
+
+const Slide = ({ image, name, category }) => (
+  <div className = {cx(CN)}>
+    <img className = {`${CN}__image`} src = {image} />
+    <h3 className = {`${CN}__product-name title-sections`} >{name}</h3>
+    <p className = {`${CN}__category`}>{category}</p>
+  </div>
+);
+
+Slide.propTypes = {
+  className: PropTypes.string,
+  image: PropTypes.string,
+  name: PropTypes.string,
+  category: PropTypes.string,
+};
+
+export default Slide;
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import ProductList from '../ProductList';
 
 const HomePage = () => (
   <div>
-    <h2>HomePage</h2>
+    <ProductList />
   </div>
 );
 
