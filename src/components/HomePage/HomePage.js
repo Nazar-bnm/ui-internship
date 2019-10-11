c8c290563e1a78f06bce1df0af4df25de488765d
Olena Kachmar <okachmar@griddynamics.com>
2019-10-11 19:01:18 +0300

    styles fixes

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+
 import ProductList from '../ProductList';
+import WhatIsNew from '../WhatIsNew';
+import BlogArticlePreview from '../BlogArticlePreview';
 
 const HomePage = () => (
   <div>
+    <WhatIsNew />
     <ProductList />
+    <BlogArticlePreview />
   </div>
 );
 
