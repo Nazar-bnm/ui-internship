b535e2f7a3aa81dc1936c8f86e18a4870329f6b2
Khrystyna Kovtun <fruitt2010@gmail.com>
2019-10-22 16:06:07 +0300

    [UIPO-41] Create accordion (#37)
    * create components and change contact details component
    
    * create accordion component
    
    * Made tests
    
    * Fix after review
    
    * Update accordion
    
    * Fix after second review
    
    * Fix after third review
    
    * Fix after forth review

---
+++ src/components/Accordion/AccordionItem/AccordionItem.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import './AccordionItem.scss';
+
+const CN = 'accordion-item';
+
+const AccordionItem = ({
+  hide,
+  onClick,
+  title,
+  description,
+  height,
+  scroll
+}) => {
+  const heightStyle = {
+    height
+  };
+
+  return (
+    <div className={CN}>
+      <div
+        className={cx(`${CN}__header`, { [`${CN}__header--active`]: !hide })}
+        onClick={onClick}
+      >
+        <h4>{title}</h4>
+        <i className={cx('caret', 'right', 'icon', { 'icon--down': !hide })} />
+      </div>
+
+      <div
+        className={cx([`${CN}__description`], {
+          [`${CN}__description--hide`]: hide,
+          [`${CN}__description--show`]: !hide,
+          scroll
+        })}
+        style={!hide ? heightStyle : {}}
+      >
+        {description}
+      </div>
+    </div>
+  );
+};
+
+AccordionItem.propTypes = {
+  hide: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  height: PropTypes.string,
+  scroll: PropTypes.bool
+};
+
+AccordionItem.defaultProps = {
+  hide: true,
+  title: '',
+  description: '',
+  height: '',
+  scroll: false
+};
+
+export default AccordionItem;
---
+++ src/components/AccordionPDP/dataItems.js
@@ -0,0 +1,22 @@
+export const accordionItemsData = [
+  {
+    id: 1,
+    title: 'description',
+    description: ` Architecto, earum temporibus quidem ex eaque unde harum sit, 
+      deleniti tempore laboriosam neque cumque!Architecto, earum temporibus 
+      quidem ex eaque unde harum sit, deleniti tempore laboriosam neque cumque!`
+  },
+  {
+    id: 2,
+    title: 'additional info',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      `
+  },
+  {
+    id: 3,
+    title: 'tags',
+    description:
+      `Earum temporibus quidem ex eaque unde harum sit, 
+      `
+  }
+];
