f994f8ba4571c59ecf6ba09c8d79281715d69a46
Olena Kachmar <okachmar@griddynamics.com>
2019-10-24 20:28:15 +0300

    fixed dropdowns

--- src/components/ProductListPage/Filters/Filters.js
+++ src/components/ProductListPage/Filters/Filters.js
@@ -1,35 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import config from '../../../../config';
 import CheckBoxListContainer from '../CheckBoxList/CheckBoxListContainer';
-import Accordeon from '../../Accordion';
+import Accordion from '../../Accordion';
 
 import './Filters.scss';
 
-const Filters = ({ category }) => {
-  const gender = config[category];
-  const categoriesNamesList = Object.keys(gender);
-  const categoriesToDisplay = categoriesNamesList.map((categoryName) => (
-    <Accordeon
-      key={categoryName}
-      heightItem="auto"
-      open
-      data={[{
-        title: categoryName,
-        id: categoryName,
-        description: <CheckBoxListContainer
-          itemsList={gender[categoryName]}
-          category={categoryName}
-        />
-      }]}
-    />
-  ));
+class Filters extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      {categoriesToDisplay}
-    </div>
-  );
-};
+    this.state = { width: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth });
+  }
+
+  render() {
+    const { category } = this.props;
+    const gender = config[category];
+    const tabletBreakpoint = 800;
+    const { width } = this.state;
+    const renderAccordion = (categoryName, index) => {
+      const open = index === 0 && width <= tabletBreakpoint;
+
+      return (
+        <Accordion
+          key={categoryName}
+          heightItem="auto"
+          open={open}
+          data={[{
+            title: categoryName,
+            id: categoryName,
+            description: <CheckBoxListContainer
+              itemsList={gender[categoryName]}
+              category={categoryName}
+            />
+          }]}
+        />
+      );
+    };
+
+    return (
+      <div>
+        {Object.keys(gender).map(renderAccordion)}
+      </div>
+    );
+  }
+}
 
 export default Filters;
