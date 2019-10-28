fa2814bceb0d86cc31421847fa67b23c0f9872e7
KhrystynaVaskovych <50922050+KhrystynaVaskovych@users.noreply.github.com>
2019-10-28 12:25:20 +0200

    [UIPO - 13 ] Create wishlist page (#36)
    * Layout and media done
    
    * Corrected imports
    
    * Resolved conflicts, Added mocked data, changed WishlistItem into functional component
    
    * Added empty lines
    
    * Changed Wishlist into functional component, changed propTypes
    
    * Changed empty div
    
    * Added classnames, changed styles
    
    * Removed extra div
    
    * Added redux-thunk, changed payload to id, refactored the code
    
    * Changed default layout
    
    * Minor changes
    
    * Fixed acc to comments

---
+++ src/components/WishlistPage/WishlistPage.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+
+import WishlistItem from './WishlistItem';
+
+import './WishlistPage.scss';
+
+const CN = 'wishlist';
+
+const WishlistPage = (props) => {
+  const {
+    wishlist, products, removeFromWishlist, className
+  } = props;
+
+  const renderNoWishlistItems = () => (
+    <div className={`${CN} ${CN}__empty-page`}>
+      <h2 className={`${CN}__subtitle`}>No items in your wishlist!</h2>
+      <img
+        className={`${CN}__image col-6 col-center"`}
+        alt="no cart items"
+        src="src/assets/img/content/img-no-cartitems.png"
+      />
+    </div>
+  );
+
+  const renderWishlistPage = () => (
+    wishlist.length ? (products.map((item) => {
+      if (wishlist.includes(item.id)) {
+        return (
+          <WishlistItem
+            key={item.id}
+            item={item}
+            removeFromWishlist={removeFromWishlist}
+          />
+        );
+      }
+    }))
+      : renderNoWishlistItems()
+  );
+
+  return (
+    <div className={cx(CN, className)}>
+      <div className={`${CN} content`}>
+        <h1 className={`${CN}__title`}>Wishlist</h1>
+      </div>
+      <div className={`${CN} content`}>
+        {renderWishlistPage()}
+      </div>
+    </div>
+  );
+};
+
+WishlistPage.propTypes = {
+  wishlist: PropTypes.array,
+  products: PropTypes.array,
+  removeFromWishlist: PropTypes.func.isRequired,
+  className: PropTypes.string
+};
+
+WishlistPage.defaultProps = {
+  className: '',
+  wishlist: [],
+  products: []
+};
+
+export default WishlistPage;
