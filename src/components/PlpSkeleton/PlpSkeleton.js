a9f09485419fdff2679e12d3b0f7ba307c2be745
KhrystynaVaskovych <khrystynavaskovych@gmail.com>
2019-10-29 17:23:31 +0200

    Fixes to plp-skeleton

--- src/components/PlpSkeleton/PlpSkeleton.js
+++ src/components/PlpSkeleton/PlpSkeleton.js
@@ -11,24 +11,17 @@ const PlpSkeleton = () => (
     <HeaderSkeleton />
     <hr className={`${CN}__line`} />
     <div className={`${CN}__nav container`}>
-        <div className={`${CN}__item`} />
+      <div className={`${CN}__item`} />
       <div className={`${CN}__item col-2 col-right`} />
     </div>
     <div className={`${CN}__content container`}>
-    <div className={`${CN}__filters col-3`}>
-    <div className={`${CN}__item`}></div>
-    <div className={`${CN}__category col-9`}></div>
-    <div className={`${CN}__category col-9`}></div>
+      <div className={`${CN}__filters col-3`}>
+        <div className={`${CN}__item`} />
+        <div className={`${CN}__category col-9`} />
+        <div className={`${CN}__category col-9`} />
+      </div>
+      <div className={`${CN}__products col-9`} />
     </div>
-
-
-
-
-    <div className={`${CN}__products col-9`}></div>
-    </div>
-
-
-
   </div>
 );
 
