364d9f0ff4eaf06cdc89f528aac37a3d6380fe9d
KhrystynaVaskovych <khrystynavaskovych@gmail.com>
2019-10-29 19:52:53 +0200

    Some fixes

---
+++ src/components/MostPopular/ProductItem/ProductItemContainer.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { addToWishlist, removeFromWishlist } from '../../../actions/actionsWishlist';
+import ProductItem from './ProductItem';
+
+const mapStateToProps = ({ reducerWishlist, productsReducer }) => ({
+  wishlist: reducerWishlist.wishlist,
+  products: productsReducer.products
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addToWishlist: bindActionCreators(addToWishlist, dispatch),
+  removeFromWishlist: bindActionCreators(removeFromWishlist, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductItem);
