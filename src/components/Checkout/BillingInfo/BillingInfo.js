b75d696b4cc7b26ede41e75e5662e5261fac9bbd
myrosvas <myrosvas@gmail.com>
2019-11-06 14:29:11 +0200

    billing info 50% done

--- src/components/Checkout/BillingInfo/BillingInfo.js
+++ src/components/Checkout/BillingInfo/BillingInfo.js
@@ -3,86 +3,86 @@ import cx from 'classnames';
 
 import { Dropdown, Button } from '@/shared';
 
+import './BillingInfo.scss';
+
 const CN = 'billing-info';
 
 class BillingInfo extends React.Component {
   render() {
     return (
       <div className={cx(CN)}>
-        <form>
+        <form className={`${CN}__form`}>
           <div className={`${CN}__input-wrapper`}>
             <label htmlFor="first-name">first name*</label>
-            <input type="text" name={CN} className={`${CN}__radiobtn-input`} />
-          </div>
-          <div className={`${CN}__input-wrapper`}>
-            <label htmlFor="company">Company</label>
-            <input type="text" name={CN} className={`${CN}__radiobtn-input`} />
-          </div>
-          <div className={`${CN}__input-wrapper`}>
-            <label htmlFor="address1">address 1</label>
-            <input type="text" name={CN} className={`${CN}__radiobtn-input`} />
-          </div>
-          <div className={`${CN}__input-wrapper`}>
-            <Dropdown />
-          </div>
-          <div className={`${CN}__input-wrapper`}>
-            <Dropdown />
-          </div>
-          <div className={`${CN}__input-wrapper`}>
-            <label htmlFor="phone">phone</label>
-            <input type="text" name={CN} className={`${CN}__radiobtn-input`} />
-          </div>
-          <div className={`${CN}__input-wrapper`}>
-            <label htmlFor="password">password*</label>
-            <input type="password" name={CN} className={`${CN}__radiobtn-input`} />
+            <input type="text" name={CN} className={`${CN}__input`} />
           </div>
           <div className={`${CN}__input-wrapper`}>
             <label htmlFor="last-name">last name*</label>
-            <input type="text" name={CN} className={`${CN}__radiobtn-input`} />
+            <input type="text" name={CN} className={`${CN}__input`} />
+          </div>
+          <div className={`${CN}__input-wrapper`}>
+            <label htmlFor="company">Company</label>
+            <input type="text" name={CN} className={`${CN}__input`} />
           </div>
           <div className={`${CN}__input-wrapper`}>
             <label htmlFor="e-mail">e-mail address*</label>
-            <input type="email" name={CN} className={`${CN}__radiobtn-input`} />
+            <input type="email" name={CN} className={`${CN}__input`} />
+          </div>
+          <div className={`${CN}__input-wrapper`}>
+            <label htmlFor="address1">address 1</label>
+            <input type="text" name={CN} className={`${CN}__input`} />
           </div>
           <div className={`${CN}__input-wrapper`}>
             <label htmlFor="address2">address 2</label>
-            <input type="text" name={CN} className={`${CN}__radiobtn-input`} />
-          </div>
-
-          <div className={`${CN}__input-wrapper`}>
-            <label htmlFor="password">password*</label>
-            <input type="password" name={CN} className={`${CN}__radiobtn-input`} />
+            <input type="text" name={CN} className={`${CN}__input`} />
           </div>
           <div className={`${CN}__input-wrapper`}>
-            <label htmlFor="city">city</label>
-            <input type="text" name={CN} className={`${CN}__radiobtn-input`} />
+            <p>country</p>
+            <Dropdown />
+          </div>
+          <div className={`${CN}__input-wrapper`}>
+            <label htmlFor="password">city</label>
+            <input type="password" name={CN} className={`${CN}__input`} />
+          </div>
+          <div className={`${CN}__input-wrapper`}>
+            <p>state</p>
+            <Dropdown />
           </div>
           <div className={`${CN}__input-wrapper`}>
             <label htmlFor="zipcode">zip / postal code*</label>
-            <input type="text" name={CN} className={`${CN}__radiobtn-input`} />
+            <input type="text" name={CN} className={`${CN}__input`} />
+          </div>
+          <div className={`${CN}__input-wrapper`}>
+            <label htmlFor="phone">phone</label>
+            <input type="text" name={CN} className={`${CN}__input`} />
           </div>
           <div className={`${CN}__input-wrapper`}>
             <label htmlFor="fax">fax</label>
-            <input type="fax" name={CN} className={`${CN}__radiobtn-input`} />
+            <input type="fax" name={CN} className={`${CN}__input`} />
+          </div>
+          <div className={`${CN}__input-wrapper`}>
+            <label htmlFor="password">password*</label>
+            <input type="password" name={CN} className={`${CN}__input`} />
           </div>
           <div className={`${CN}__input-wrapper`}>
             <label htmlFor="confirm-password">confirm password*</label>
-            <input type="confirm-password" name={CN} className={`${CN}__radiobtn-input`} />
+            <input type="confirm-password" name={CN} className={`${CN}__input`} />
           </div>
         </form>
-        <form>
+        <form className={`${CN}__radiobtns-form`}>
           <div className={`${CN}__radiobtn`}>
-            <input type="radio" name="ship-to-this-address" value="ship-to-this-address" className={`${CN}__radiobtn-input`} />
+            <input className={`${CN}__radiobtn-input`} type="radio" name="ship-to-this-address" value="ship-to-this-address" />
             <label htmlFor="ship-to-this-address">Ship to this address</label>
           </div>
           <div className={`${CN}__radiobtn`}>
-            <input type="radio" name="ship-to-this-address" value="ship-to-different-address" className={`${CN}__radiobtn-input`} />
-            <label htmlFor="ship-to-different-address">Ship to this address</label>
+            <input className={`${CN}__radiobtn-input`} type="radio" name="ship-to-this-address" value="ship-to-different-address" />
             <label htmlFor="guest">Ship to a different address</label>
           </div>
         </form>
-        <input type="checkbox" />
-        I want to subscribe to the newsletter
+        <div className={`${CN}__checkbox-wrapper`}>
+          <input type="checkbox" />
+          I want to subscribe to the newsletter
+        </div>
         <Button customClass="login-register__button">continue</Button>
       </div>
     );
--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -4,10 +4,10 @@ import cx from 'classnames';
 import Accordion from '../Accordion';
 import LoginRegister from './LoginRegister';
 import BillingInfo from './BillingInfo';
-// import ShippingInfo from './ShippingInfo';
-// import ShippingMethod from './ShippingMethod';
-// import Payment from './Payment';
-// import OrderReview from './OrderReview';
+import ShippingInfo from './ShippingInfo';
+import ShippingMethod from './ShippingMethod';
+import Payment from './Payment';
+import OrderReview from './OrderReview';
 
 import './Checkout.scss';
 
@@ -22,27 +22,27 @@ const data = [
     title: '02. Billing Info',
     id: 2,
     description: <BillingInfo />
+  },
+  {
+    title: '03. Shipping Info',
+    id: 3,
+    description: <ShippingInfo />
+  },
+  {
+    title: '04. Shipping Method',
+    id: 4,
+    description: <ShippingMethod />
+  },
+  {
+    title: '05. Payment',
+    id: 5,
+    description: <Payment />
+  },
+  {
+    title: '06. Order review',
+    id: 6,
+    description: <OrderReview />
   }
-  // {
-  //   title: '03. Shipping Info',
-  //   id: 3,
-  //   description: <ShippingInfo />
-  // },
-  // {
-  //   title: '04. Shipping Method',
-  //   id: 4,
-  //   description: <ShippingMethod />
-  // },
-  // {
-  //   title: '05. Payment',
-  //   id: 5,
-  //   description: <Payment />
-  // },
-  // {
-  //   title: '06. Order review',
-  //   id: 6,
-  //   description: <OrderReview />
-  // }
 ];
 // console.log('data ', data);
 // console.log(data)
