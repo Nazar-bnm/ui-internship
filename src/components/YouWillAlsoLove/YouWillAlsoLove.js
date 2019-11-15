703a4ef3c03e788d7f6ae4176f2380b655673cc8
mvasylykiv <mvasylykiv@griddynamics.com>
2019-11-15 16:19:05 +0200

    modal window added

--- src/components/YouWillAlsoLove/YouWillAlsoLove.js
+++ src/components/YouWillAlsoLove/YouWillAlsoLove.js
@@ -1,29 +1,87 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { Button } from '@/shared';
+import Modal from '../Modal';
+import Heading from '../Heading';
+import ProductImage from '../ProductImage';
+import ProductOrder from '../ProductOrder';
+
 import './YouWillAlsoLove.scss';
 
 const CN = 'you-will-also-love';
 
-const YouWillAlsoLove = (props) => {
-  const { userCart, products } = props;
+class YouWillAlsoLove extends React.Component {
+  constructor(props) {
+    super(props);
 
-  if (!userCart.length) {
+    this.state = {
+      isShowedModal: false
+    };
+
+    this.showModal = this.showModal.bind(this);
+    this.removeModal = this.removeModal.bind(this);
+    this.renderQuickView = this.renderQuickView.bind(this);
+  }
+
+  showModal() {
+    this.setState({
+      isShowedModal: true
+    });
+    this.renderQuickView();
+  }
+
+  removeModal() {
+    this.setState({
+      isShowedModal: false
+    });
+  }
+
+  renderQuickView(item) {
+    const {
+      images,
+      _id
+    } = item;
+
+    const imagesForQuickView = images.map(({ claudinaryId }) => ({
+      src: `${process.env.IMAGE_URL}/${claudinaryId}`
+    }));
+
     return (
-      <div className={`${CN} content`}>
-        <h2 className={`${CN}__title`}>you will also love</h2>
-        Buy something please!
-      </div>
+      <Modal className={`${CN}-quick-view`} removeModal={this.removeModal}>
+        <Heading title="Quick view" />
+        <div className={`${CN}-quick-view-content`}>
+          <ProductImage className={`${CN}-quick-view-content-images`} images={imagesForQuickView} verticalCarousel />
+          <div className={`${CN}-quick-view-content-wrapper`}>
+            <ProductOrder
+              id={_id}
+              className={`${CN}-quick-view-content__product-order`}
+            />
+            <NavLink
+              to={`/product-details/${_id}`}
+              className={`${CN}-quick-view-content__link`}
+            >
+              See more details
+            </NavLink>
+          </div>
+        </div>
+      </Modal>
     );
   }
 
-  if (userCart.length) {
+  render() {
+    const { userCart, products } = this.props;
+
+    if (!userCart.length || window.innerWidth < 1024) {
+      return null;
+    }
     const selectedGender = userCart[0].data.genders[0];
     const selectedCategory = userCart[0].data.category;
     const selectedSize = userCart[0].data.sizes[0];
     const productsToRender = products.filter((item) => item.category === selectedCategory && item.genders.includes(selectedGender) && item.sizes.includes(selectedSize));
     const itemsToRenderNumber = 3;
     const croppedProducts = productsToRender.slice(0, itemsToRenderNumber);
+    const { isShowedModal } = this.state;
 
     const renderProducts = () => (
       croppedProducts.map((item) => {
@@ -42,8 +100,9 @@ const YouWillAlsoLove = (props) => {
             <div className={`${CN}__item__description`}>
               <h3 className={`${CN}__item__title`}>{title}</h3>
               <span className={`${CN}__item__price`}>{priceToRender}</span>
-              <Button className={`${CN}__quickview-button`}>quickview</Button>
+              <Button className={`${CN}__quickview-button`} onClick={this.showModal}>quickview</Button>
             </div>
+            {isShowedModal && this.renderQuickView(item)}
           </div>
         );
       })
@@ -56,6 +115,6 @@ const YouWillAlsoLove = (props) => {
       </div>
     );
   }
-};
+}
 
 export default YouWillAlsoLove;
