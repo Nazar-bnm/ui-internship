ace802992f5a03fa223df3b0a5ab4cf3d927b699
mvasylykiv <mvasylykiv@griddynamics.com>
2019-11-18 13:28:39 +0200

    fixed modal window rendering

--- src/components/YouWillAlsoLove/YouWillAlsoLove.js
+++ src/components/YouWillAlsoLove/YouWillAlsoLove.js
@@ -16,17 +16,18 @@ class YouWillAlsoLove extends React.Component {
     super(props);
 
     this.state = {
-      isShowedModal: false
+      isShowedModal: false,
+      selectedItem: {}
     };
 
-    this.showModal = this.showModal.bind(this);
     this.removeModal = this.removeModal.bind(this);
     this.renderQuickView = this.renderQuickView.bind(this);
   }
 
-  showModal() {
+  handleClick(item) {
     this.setState({
-      isShowedModal: true
+      isShowedModal: true,
+      selectedItem: item
     });
     this.renderQuickView();
   }
@@ -37,38 +38,36 @@ class YouWillAlsoLove extends React.Component {
     });
   }
 
-  renderQuickView(item) {
-    if (item) {
-      const {
-        images,
-        _id
-      } = item;
+  renderQuickView() {
+    const { selectedItem } = this.state;
+    const {
+      images,
+      _id
+    } = selectedItem;
+    const imagesForQuickView = images.map(({ claudinaryId }) => ({
+      src: `${process.env.IMAGE_URL}/${claudinaryId}`
+    }));
 
-      const imagesForQuickView = images.map(({ claudinaryId }) => ({
-        src: `${process.env.IMAGE_URL}/${claudinaryId}`
-      }));
-
-      return (
-        <Modal className={`${CN}-quick-view`} removeModal={this.removeModal}>
-          <Heading title="Quick view" />
-          <div className={`${CN}-quick-view-content`}>
-            <ProductImage className={`${CN}-quick-view-content-images`} images={imagesForQuickView} verticalCarousel />
-            <div className={`${CN}-quick-view-content-wrapper`}>
-              <ProductOrder
-                id={_id}
-                className={`${CN}-quick-view-content__product-order`}
-              />
-              <NavLink
-                to={`/product-details/${_id}`}
-                className={`${CN}-quick-view-content__link`}
-              >
-                See more details
-              </NavLink>
-            </div>
+    return (
+      <Modal className={`${CN}-quick-view`} removeModal={this.removeModal}>
+        <Heading title="Quick view" />
+        <div className={`${CN}-quick-view-content`}>
+          <ProductImage className={`${CN}-quick-view-content-images`} images={imagesForQuickView} verticalCarousel />
+          <div className={`${CN}-quick-view-content-wrapper`}>
+            <ProductOrder
+              id={_id}
+              className={`${CN}-quick-view-content__product-order`}
+            />
+            <NavLink
+              to={`/product-details/${_id}`}
+              className={`${CN}-quick-view-content__link`}
+            >
+              See more details
+            </NavLink>
           </div>
-        </Modal>
-      );
-    }
+        </div>
+      </Modal>
+    );
   }
 
   render() {
@@ -83,7 +82,7 @@ class YouWillAlsoLove extends React.Component {
     const productsToRender = products.filter((item) => item.category === selectedCategory && item.genders.includes(selectedGender) && item.sizes.includes(selectedSize));
     const itemsToRenderNumber = 3;
     const croppedProducts = productsToRender.slice(0, itemsToRenderNumber);
-    const { isShowedModal } = this.state;
+    const { selectedItem, isShowedModal } = this.state;
 
     const renderProducts = () => (
       croppedProducts.map((item) => {
@@ -93,7 +92,7 @@ class YouWillAlsoLove extends React.Component {
         const imageSrc = `${process.env.IMAGE_URL}/${imageClaudinaryId}`;
 
         return (
-          <div className={`${CN}__item`}>
+          <div key={title} className={`${CN}__item`}>
             <img
               alt="product"
               className={`${CN}__image`}
@@ -102,9 +101,8 @@ class YouWillAlsoLove extends React.Component {
             <div className={`${CN}__item__description`}>
               <h3 className={`${CN}__item__title`}>{title}</h3>
               <span className={`${CN}__item__price`}>{priceToRender}</span>
-              <Button className={`${CN}__quickview-button`} onClick={this.showModal}>quickview</Button>
+              <Button className={`${CN}__quickview-button`} onClick={this.handleClick.bind(this, item)}>quickview</Button>
             </div>
-            {isShowedModal && this.renderQuickView(item)}
           </div>
         );
       })
@@ -114,6 +112,7 @@ class YouWillAlsoLove extends React.Component {
       <div className={`${CN} content`}>
         <h2 className={`${CN}__title`}>you will also love</h2>
         {renderProducts()}
+        {isShowedModal && this.renderQuickView(selectedItem)}
       </div>
     );
   }
