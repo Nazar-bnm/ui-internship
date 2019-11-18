304e11f74cc211862a22ce36535661b1d4b7a603
mvasylykiv <mvasylykiv@griddynamics.com>
2019-11-18 15:26:33 +0200

    refactoring

--- src/components/YouWillAlsoLove/YouWillAlsoLove.js
+++ src/components/YouWillAlsoLove/YouWillAlsoLove.js
@@ -21,7 +21,22 @@ class YouWillAlsoLove extends React.Component {
     };
 
     this.removeModal = this.removeModal.bind(this);
+    this.getProductsToRender = this.getProductsToRender.bind(this);
     this.renderQuickView = this.renderQuickView.bind(this);
+    this.renderTreeItems = this.renderTreeItems.bind(this);
+  }
+
+  getProductsToRender() {
+    const { userCart, products } = this.props;
+    const itemsToRenderNumber = 3;
+    const firstItemInCart = userCart[0];
+    const selectedGender = firstItemInCart.data.genders[0];
+    const selectedCategory = firstItemInCart.data.category;
+    const selectedSize = firstItemInCart.data.sizes[0];
+    const productsToRender = products.filter((item) => item.category === selectedCategory && item.genders.includes(selectedGender) && item.sizes.includes(selectedSize));
+    const croppedProducts = productsToRender.slice(0, itemsToRenderNumber);
+
+    return croppedProducts;
   }
 
   handleClick(item) {
@@ -40,10 +55,7 @@ class YouWillAlsoLove extends React.Component {
 
   renderQuickView() {
     const { selectedItem } = this.state;
-    const {
-      images,
-      _id
-    } = selectedItem;
+    const { images, _id } = selectedItem;
     const imagesForQuickView = images.map(({ claudinaryId }) => ({
       src: `${process.env.IMAGE_URL}/${claudinaryId}`
     }));
@@ -52,7 +64,11 @@ class YouWillAlsoLove extends React.Component {
       <Modal className={`${CN}-quick-view`} removeModal={this.removeModal}>
         <Heading title="Quick view" />
         <div className={`${CN}-quick-view-content`}>
-          <ProductImage className={`${CN}-quick-view-content-images`} images={imagesForQuickView} verticalCarousel />
+          <ProductImage
+            className={`${CN}-quick-view-content-images`}
+            images={imagesForQuickView}
+            verticalCarousel
+          />
           <div className={`${CN}-quick-view-content-wrapper`}>
             <ProductOrder
               id={_id}
@@ -70,49 +86,51 @@ class YouWillAlsoLove extends React.Component {
     );
   }
 
+  renderTreeItems() {
+    const croppedProducts = this.getProductsToRender();
+    const productsToRender = croppedProducts.map((item) => {
+      const { title, price } = item;
+      const imageClaudinaryId = item.images[0].claudinaryId;
+      const priceToRender = `$${price}.00`;
+      const imageSrc = `${process.env.IMAGE_URL}/${imageClaudinaryId}`;
+
+      return (
+        <div key={title} className={`${CN}__item`}>
+          <img
+            alt="product"
+            className={`${CN}__image`}
+            src={imageSrc}
+          />
+          <div className={`${CN}__item__description`}>
+            <h3 className={`${CN}__item__title`}>{title}</h3>
+            <span className={`${CN}__item__price`}>{priceToRender}</span>
+            <Button
+              className={`${CN}__quickview-button`}
+              onClick={this.handleClick.bind(this, item)}
+            >
+              quickview
+            </Button>
+          </div>
+        </div>
+      );
+    });
+
+    return productsToRender;
+  }
+
   render() {
-    const { userCart, products } = this.props;
+    const { userCart } = this.props;
+    const { selectedItem, isShowedModal } = this.state;
 
     if (!userCart.length || window.innerWidth < 1024) {
       return null;
     }
-    const selectedGender = userCart[0].data.genders[0];
-    const selectedCategory = userCart[0].data.category;
-    const selectedSize = userCart[0].data.sizes[0];
-    const productsToRender = products.filter((item) => item.category === selectedCategory && item.genders.includes(selectedGender) && item.sizes.includes(selectedSize));
-    const itemsToRenderNumber = 3;
-    const croppedProducts = productsToRender.slice(0, itemsToRenderNumber);
-    const { selectedItem, isShowedModal } = this.state;
-
-    const renderProducts = () => (
-      croppedProducts.map((item) => {
-        const { title, price } = item;
-        const imageClaudinaryId = item.images[0].claudinaryId;
-        const priceToRender = `$${price}.00`;
-        const imageSrc = `${process.env.IMAGE_URL}/${imageClaudinaryId}`;
-
-        return (
-          <div key={title} className={`${CN}__item`}>
-            <img
-              alt="product"
-              className={`${CN}__image`}
-              src={imageSrc}
-            />
-            <div className={`${CN}__item__description`}>
-              <h3 className={`${CN}__item__title`}>{title}</h3>
-              <span className={`${CN}__item__price`}>{priceToRender}</span>
-              <Button className={`${CN}__quickview-button`} onClick={this.handleClick.bind(this, item)}>quickview</Button>
-            </div>
-          </div>
-        );
-      })
-    );
 
     return (
       <div className={`${CN} content`}>
         <h2 className={`${CN}__title`}>you will also love</h2>
-        {renderProducts()}
-        {isShowedModal && this.renderQuickView(selectedItem)}
+        {this.renderTreeItems()}
+        {isShowedModal && this.renderQuickView.bind(this, selectedItem)()}
       </div>
     );
   }
