550c46c23fb0a7f7356efb3b3b948f29b46579dd
Vasyl <vandrela@griddynamics.com>
2019-11-25 17:50:24 +0200

    added validation but not finished

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,23 +1,45 @@
+/* eslint-disable no-console */
 import React, { Component } from 'react';
 import ls from 'local-storage';
 import { Link } from 'react-router-dom';
-
-import { Button } from '@/shared';
 
 import './RegisterForm.scss';
 
 const CN = 'register-form';
 
+const emailRegex = RegExp(
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+);
+
+const formValid = ({ formErrors, ...rest }) => {
+  let valid = true;
+
+  Object.values(formErrors).forEach((val) => {
+    val.length > 0 && (valid = false);
+  });
+
+  Object.values(rest).forEach((val) => {
+    val === null && (valid = false);
+  });
+
+  return valid;
+};
+
 class RegisterForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: ''
-      // password: '',
-      // name: ''
+      firstName: null,
+      lastName: null,
+      email: null,
+      password: null,
+      formErrors: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+      }
     };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -26,59 +48,132 @@ class RegisterForm extends Component {
     });
   }
 
-  saveToLocalStore() {
-
-  }
-
-  handleSubmit(e) {
+  handleChange = (e) => {
     e.preventDefault();
-    const form = e.target;
-    const data = new FormData(form);
+    const { name, value } = e.target;
+    const { formErrors } = this.state;
 
-    const URL = '/api/form-submit-url';
+    switch (name) {
+      case 'firstName':
+        formErrors.firstName = value.length < 3 ? 'minimum 3 characaters required' : '';
+        break;
+      case 'lastName':
+        formErrors.lastName = value.length < 3 ? 'minimum 3 characaters required' : '';
+        break;
+      case 'email':
+        formErrors.email = emailRegex.test(value)
+          ? ''
+          : 'invalid email address';
+        break;
+      case 'password':
+        formErrors.password = value.length < 6 ? 'minimum 6 characaters required' : '';
+        break;
+      default:
+        break;
+    }
 
-    fetch(URL, {
-      method: 'POST',
-      body: data
-    });
+    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // const form = e.target;
+    // const data = new FormData(form);
+
+    // const URL = '/api/form-submit-url';
+
+    // fetch(URL, {
+    //   method: 'POST',
+    //   body: data
+    // });
+
+    const {
+      firstName,
+      lastName,
+      email,
+      password
+    } = this.state;
+
+    if (formValid(this.state)) {
+      console.log(`
+        First Name: ${firstName}
+        Last Name: ${lastName}
+        Email: ${email}
+        Password: ${password}
+      `);
+    } else {
+      console.error('FORM INVALID - DISPLAY ERROR MESSAGE');
+    }
   }
 
   render() {
     const { email } = this.state;
+    const { formErrors } = this.state;
 
     return (
-      <form className={`${CN}`} onSubmit={this.handleSubmit}>
+      <form className={`${CN}`} onSubmit={this.handleSubmit} noValidate>
         <span className={`${CN}__label`}>create an account</span>
         <div className={`${CN}__wrapper`}>
           <div className={`${CN}__personal-information ${CN}__wrapper__personal-information`}>
 
             <h3>personal information</h3>
             <div className={`${CN}__field`}>
-              <label className={`${CN}__field__label`}>first name*</label>
-              <input className={`${CN}__field__input`} type="text" required />
+              <label htmlFor="firstName" className={`${CN}__field__label`}>first name*</label>
+              <input
+                name="firstName"
+                className={`${CN}__field__input`}
+                type="text"
+                noValidate
+                onChange={this.handleChange}
+              />
             </div>
+            {formErrors.firstName.length > 0 && (
+              <span className="errorMessage">{formErrors.firstName}</span>
+            )}
 
             <div className={`${CN}__field`}>
               <label className={`${CN}__field__label`}>last name*</label>
-              <input className={`${CN}__field__input`} type="text" required />
+              <input
+                name="lastName"
+                className={`${CN}__field__input`}
+                type="text"
+                noValidate
+                onChange={this.handleChange}
+              />
             </div>
+            {formErrors.lastName.length > 0 && (
+              <span className="errorMessage">{formErrors.lastName}</span>
+            )}
 
             <div className={`${CN}__field`}>
               <label className={`${CN}__field__label`}>e-mail*</label>
               <input
+                name="email"
                 className={`${CN}__field__input`}
                 type="email"
                 id="email"
-                value={email}
-                required
+                defaultValue={email}
+                noValidate
+                onChange={this.handleChange}
               />
             </div>
+            {formErrors.email.length > 0 && (
+              <span className="errorMessage">{formErrors.email}</span>
+            )}
 
             <div className={`${CN}__field`}>
               <label className={`${CN}__field__label`}>password*</label>
-              <input className={`${CN}__field__input`} type="password" required />
+              <input
+                name="password"
+                className={`${CN}__field__input`}
+                type="password"
+                noValidate
+                onChange={this.handleChange}
+              />
             </div>
-
+            {formErrors.password.length > 0 && (
+              <span className="errorMessage">{formErrors.password}</span>
+            )}
           </div>
 
           <div className={`${CN}__address-information ${CN}__wrapper__address-information`}>
@@ -110,21 +205,21 @@ class RegisterForm extends Component {
             </div>
 
             <div className={`${CN}__field`}>
-              <label className={`${CN}__field__label`}>zip/postal code*</label>
-              <input className={`${CN}__field__input`} type="text" required />
+              <label className={`${CN}__field__label`}>zip/postal code</label>
+              <input className={`${CN}__field__input`} type="text" />
             </div>
 
             <div className={`${CN}__field`}>
               <label className={`${CN}__field__label`}>phone</label>
-              <input className={`${CN}__field__input`} type="text" />
+              <input className={`${CN}__field__input`} type="number" />
             </div>
           </div>
         </div>
 
         <div className={`${CN}__buttons-wrapper`}>
-          <Button className="black-button">
+          <button type="submit" className="black-button">
             create an account
-          </Button>
+          </button>
           <Link to="/login">
             Back to login
           </Link>
