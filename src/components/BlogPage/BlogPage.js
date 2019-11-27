118ffce360ffce6f72776a9c3ed28e43e8f2dbb3
Khrystyna <fruitt2010@gmail.com>
2019-11-27 12:41:48 +0200

    Add filter for blogs

--- src/components/BlogPage/BlogPage.js
+++ src/components/BlogPage/BlogPage.js
@@ -1,10 +1,14 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-to-interactive-role */
 import React from 'react';
+import cx from 'classnames';
 import HttpService from '../../service/HttpService/httpService';
 
 import BlogItem from './BlogItem';
+import Preloader from '../Preloader';
 import { Button } from '@/shared';
 import Labels from '../Labels';
 import { formatDate } from '../../helpers';
+import { blogCategories } from '../../constants';
 
 import './BlogPage.scss';
 
@@ -15,29 +19,47 @@ class BlogPage extends React.Component {
     super(props);
 
     this.state = {
+      isLoading: false,
+      category: '',
       currentRangeStart: 0,
       currentRangeEnd: 3,
       isButtonDisabled: false
     };
 
     this.getOlderPosts = this.getOlderPosts.bind(this);
+    this.setCategory = this.setCategory.bind(this);
   }
 
   componentDidMount() {
     this.getBlogItems();
   }
 
-  async getBlogItems() {
+  componentDidUpdate(prevProps, { category: prevCategory }) {
+    const { category } = this.state;
+
+    category !== prevCategory && this.getBlogItems(category);
+  }
+
+  async getBlogItems(category) {
+    let query = '';
+    this.setState({ isLoading: true });
+    if (category) {
+      query = `?category=${category}`;
+    }
+
     const userAPI = new HttpService();
-    const blogsURL = `${process.env.SERVER_URL}/blogs`;
+    const blogsURL = `${process.env.SERVER_URL}/blogs${query}`;
     const { getBlogItemsSuccess, getBlogItemsError } = this.props;
 
     try {
       const response = await userAPI.get(blogsURL);
 
+      setTimeout(() => this.setState({ isLoading: false }), 500);
+
       if (response.status === 404) {
         throw Error(response.statusText);
       }
+
       getBlogItemsSuccess(response.data);
     } catch (error) {
       getBlogItemsError(error);
@@ -47,17 +69,17 @@ class BlogPage extends React.Component {
   getItemsToRender() {
     const { currentRangeStart, currentRangeEnd } = this.state;
     const { blogItems } = this.props;
-    const blogItemsToRender = blogItems.slice(currentRangeStart, currentRangeEnd);
+    const blogItemsToRender = blogItems.slice(
+      currentRangeStart,
+      currentRangeEnd
+    );
     const maxSymbolsNumber = 301;
 
     return blogItemsToRender.map((el, index) => {
       const shortText = `${el.description.slice(0, maxSymbolsNumber)}...`;
       const imageSrc = `${process.env.BLOG_IMAGE_URL}/${el.photo}`;
       const {
-        _id,
-        title,
-        date,
-        labels
+        _id, title, date, labels
       } = el;
 
       const formattedDate = formatDate(date);
@@ -97,24 +119,48 @@ class BlogPage extends React.Component {
     }
   }
 
+  setCategory(e) {
+    const value = e.target.innerHTML;
+
+    this.setState(({ category: prevCategory }) => ({
+      category: prevCategory !== value ? value : ''
+    }));
+  }
+
   render() {
-    const { isButtonDisabled } = this.state;
+    const { isButtonDisabled, isLoading, category } = this.state;
     const { blogItems, getLabel } = this.props;
+
+    const categories = blogCategories.map((categoryName) => (
+      <li
+        role="button"
+        tabIndex={0}
+        key={categoryName}
+        className={cx('categories__item', {
+          'categories__item--hovered': category === categoryName
+        })}
+        onClick={this.setCategory}
+      >
+        {categoryName}
+      </li>
+    ));
 
     return (
       <div className={`${CN} content`}>
         <div className="categories">
           <p>Categories</p>
           <hr />
-          <p>Celebrity style (39)</p>
-          <p>Fashion shows (15)</p>
-          <p>Shopping (27)</p>
-          <p>Shopping (119)</p>
+          <ul>{categories}</ul>
         </div>
         <div className="labels__wrapper">
           <Labels blogItems={blogItems} getLabel={getLabel} />
         </div>
         <div className={`${CN}__items-wrapper`}>
+          {isLoading && (
+            <div className={`${CN}--preloader`}>
+              <Preloader />
+            </div>
+          )}
           {this.getItemsToRender()}
           <Button
             disabled={isButtonDisabled}
