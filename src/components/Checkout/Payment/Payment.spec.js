166f1782b41a28762ba0c405bda5d4a41881a343
Vasyl <vandrela@griddynamics.com>
2019-11-27 17:05:43 +0200

    added tests

--- src/components/Checkout/Payment/Payment.spec.js
+++ src/components/Checkout/Payment/Payment.spec.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 import Payment from './Payment';
 
 describe('<OrderPreview />', () => {
-  let wrapper = shallow(<Payment />);
+  let wrapper = shallow(<Payment totalCount={10} showMessage={() => {}} />);
   
   test('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
